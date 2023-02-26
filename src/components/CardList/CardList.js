1b38d0caa0196b8ec737c830e6db8f6d753609d5
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-02-25 16:50:59 -0800

    pre-webpack config

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import './CardList.css'
+import dotenv from "dotenv";
+dotenv.config()
 
-const apiUrl="https://api.rawg.io/api/games?key=32de51e296df41328d00a906b8ce6b29"
+// const apiUrl={REACT_APP_API_URL}
 
 export default function CardList() {
     const [data, setData] = useState(null)
